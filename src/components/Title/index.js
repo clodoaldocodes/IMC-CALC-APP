c7671e4f6e7e1fcb4dde8ea6a975f390ee5cbe91
Clodoaldo de S. Faria Júnior <61160368+clodoaldocodes@users.noreply.github.com>
2023-04-01 15:03:30 -0300

    Only change the name app to portuguese
    Besides the app description and other things are in English, the interface app was builded all in portuguese

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -5,7 +5,7 @@ import styles from "./style";
 export default function Title(){
     return(
         <View style={styles.boxTitle}>
-            <Text style={styles.textTile}>IMC CALCULATOR</Text>
+            <Text style={styles.textTile}>CALCULADORA DE IMC</Text>
         </View>
     );
 }
